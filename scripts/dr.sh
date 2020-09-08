898b5e4c050f73afaa7a8632cea78744d2a7f453
Luca Heltai <luca.heltai@sissa.it>
2020-09-08 16:43:07 +0000

    fixed dr.sh

--- scripts/dr.sh
+++ scripts/dr.sh
@@ -1,5 +1,5 @@
 IMG=`grep -m 1 image .gitlab-ci.yml | awk '{print $2}'` 
 echo $IMG
 docker run  --user $(id -u):$(id -g) \
-    --rm -t -i \
+    --rm -t \
     -v `pwd`:/builds/app $IMG /bin/sh -c "cd /builds/app; $@"
