d44094f71606ce42959ca5861ca6dc1cd05188bf
Luca Heltai <luca.heltai@unipi.it>
2024-10-08 18:26:28 +0200

    Updated copyrigth.

--- include/laplacian.h
+++ include/laplacian.h
@@ -1,22 +1,19 @@
-/* ---------------------------------------------------------------------
- *
- * Copyright (C) 2000 - 2020 by the deal.II authors
- *
- * This file is part of the deal.II library.
- *
- * The deal.II library is free software; you can use it, redistribute
- * it, and/or modify it under the terms of the GNU Lesser General
- * Public License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * The full text of the license can be found in the file LICENSE.md at
- * the top level directory of deal.II.
- *
- * ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2024 by Luca Heltai
+//
+// This file is part of the bare-dealii-app application, based on the deal.II library.
+//
+//  The bare-dealii-app application is free software; you can use it, redistribute it,
+//  and/or modify it under the terms of the Apache-2.0 License WITH
+//  LLVM-exception as published by the Free Software Foundation; either version
+//  3.0 of the License, or (at your option) any later version. The full text of
+//  the license can be found in the file LICENSE at the top level of the 
+//  bare-dealii-app distribution. 
+// 
+// ---------------------------------------------------------------------
 
- *
- * Author: Wolfgang Bangerth, University of Heidelberg, 2000
- * Modified by: Luca Heltai, 2020
- */
+
 #ifndef dealii_laplacian_h
 #define dealii_laplacian_h
 
--- tests/template.cc
+++ tests/template.cc
@@ -1,13 +1,17 @@
-//-----------------------------------------------------------
+// ---------------------------------------------------------------------
 //
-//    Copyright (C) 2020 by the deal.II authors
+// Copyright (C) 2024 by Luca Heltai
 //
-//    This file is subject to LGPL and may not be distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
+// This file is part of the bare-dealii-app application, based on the deal.II library.
 //
-//-----------------------------------------------------------
+//  The bare-dealii-app application is free software; you can use it, redistribute it,
+//  and/or modify it under the terms of the Apache-2.0 License WITH
+//  LLVM-exception as published by the Free Software Foundation; either version
+//  3.0 of the License, or (at your option) any later version. The full text of
+//  the license can be found in the file LICENSE at the top level of the 
+//  bare-dealii-app distribution. 
+// 
+// ---------------------------------------------------------------------
 
 #include "tests.h"
 
