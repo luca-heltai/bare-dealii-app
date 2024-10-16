0e4b792507c94d28964b20d5177ca6c9e25ca81b
Michał Wichrowski <mtwichrowski@gmail.com>
2024-10-16 14:01:05 +0200

    New directory: execs. Dimension-dependent compilation of the library (parallel compilation)

---
+++ execs/example.cc
@@ -0,0 +1,55 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2024 by Luca Heltai
+//
+// This file is part of the bare-dealii-app application, based on the 
+// deal.II library.
+//
+// The bare-dealii-app application is free software; you can use it, 
+// redistribute it, and/or modify it under the terms of the Apache-2.0 License
+// WITH LLVM-exception as published by the Free Software Foundation; either 
+// version 3.0 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md
+// at the top level of the bare-dealii-app distribution. 
+// 
+// ---------------------------------------------------------------------
+
+#include "laplacian.h"
+
+int
+main()
+{
+  try
+    {
+      Laplacian<DEAL_DIMENSION> laplace_problem;
+      laplace_problem.run();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+
+  return 0;
+}
--- source/laplacian.cc
+++ source/laplacian.cc
@@ -2,16 +2,16 @@
 //
 // Copyright (C) 2024 by Luca Heltai
 //
-// This file is part of the bare-dealii-app application, based on the 
+// This file is part of the bare-dealii-app application, based on the
 // deal.II library.
 //
-// The bare-dealii-app application is free software; you can use it, 
+// The bare-dealii-app application is free software; you can use it,
 // redistribute it, and/or modify it under the terms of the Apache-2.0 License
-// WITH LLVM-exception as published by the Free Software Foundation; either 
+// WITH LLVM-exception as published by the Free Software Foundation; either
 // version 3.0 of the License, or (at your option) any later version.
 // The full text of the license can be found in the file LICENSE.md
-// at the top level of the bare-dealii-app distribution. 
-// 
+// at the top level of the bare-dealii-app distribution.
+//
 // ---------------------------------------------------------------------
 
 #include "laplacian.h"
@@ -248,6 +248,4 @@ Laplacian<dim>::run()
 }
 
 
-template class Laplacian<1>;
-template class Laplacian<2>;
-template class Laplacian<3>;
+template class Laplacian<DEAL_DIMENSION>;
